0a702841073efe146952c5f1246e6b9ad8827693
Richard <richard.verheyen@gmail.com>
2023-06-07 06:23:01 +1000

    Add a gql function and query down some results

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,37 @@
 import './App.css'
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://tmdb-one-blue.vercel.app/',
+  cache: new InMemoryCache(),
+});
 
 function App() {
+
+  function handleClick() {
+    client
+      .query({
+        query: gql`
+          query {
+            searchPerson(query: "Tom", page: 1) {
+              page
+              total_results
+              total_pages
+              results {
+                name
+              }
+            }
+          }
+        `,
+      })
+      .then((result) => console.log(result));
+  }
   return (
     <>
       <TextField id="outlined-basic" label="Search" variant="outlined" data-testid="search" />
-      <Button variant="outlined" data-testid="lucky">I'm feeling Cruisey</Button>
+      <Button onClick={handleClick} variant="outlined" data-testid="lucky">I'm feeling Cruisey</Button>
     </>
   )
 }
