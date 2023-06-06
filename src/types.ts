d725dd520e4cbf5c3755d13ac9f41eecd28d1239
Richard <richard.verheyen@gmail.com>
2023-06-07 08:13:26 +1000

    Add types:

---
+++ src/types.ts
@@ -0,0 +1,36 @@
+type Movie = {
+    id: Number
+    title: String
+    original_title: String
+    name: String
+    overview: String
+    release_date: String
+    poster_path: String
+    backdrop_path: String
+    runtime: Number
+    homepage: String
+    genres: [Genre]
+    recommendations: [Movie]
+  }
+
+type Person = {
+    id: Number
+    name: String
+    biography: String
+    birthday: String
+    deathday: String
+    place_of_birth: String
+    profile_path: String
+    cast: [Movie]
+}
+
+type Genre = {
+    id: Number
+        name: String
+    }
+
+export type {
+    Movie,
+    Person,
+    Genre
+}
